be0174b30e30564453046df954dd0cc520127a98
haneen <hanen.m.mhmed@gmail.com>
2025-04-20 18:56:29 +0200

    feat : create , get function

---
+++ db_crud.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+
+mongo=MongoClient("mongodb://localhost:27017/")
+
+db=mongo["Mongo_CRUD"]
+collection=db["Data"]
+
+def insert(data:dict):
+    collection.insert_one(data)
+    return {"message": "Data inserted successfully"}
+
+def get(name: str):
+    doc = collection.find_one({"name": name})
+    if doc and "_id" in doc:
+        del doc["_id"]
+    return doc
